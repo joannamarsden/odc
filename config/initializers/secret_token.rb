ac15854a71071bcab9dc14446c3b379a4e80029a
Joanna Marsden <joanna.marsden@gmail.com>
2014-03-29 11:31:49 -0400

    first commit & added foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Odc::Application.config.secret_token = '1a446b126d29b54174a5819a229677a533ea7d8d07e991ac00d35839bcb9eeacc5327faf656955782b8e6a2723476ad4301fc14289a794fe74a018dc03e49a07'
