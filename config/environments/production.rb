7be9d9138e52dc5793a2f5c463fd1cb4bd9a1563
Joanna Marsden <joanna.marsden@gmail.com>
2014-03-29 15:17:04 -0400

    site name and bilingual cms

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,8 @@ Odc::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  # Precompile additional assets with modernizr
+ config.assets.precompile += %w( vendor/modernizr.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   if config.respond_to?(:action_mailer)
