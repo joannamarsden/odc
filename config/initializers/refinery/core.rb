e590526e02fa40ed91136ba4c9797ec379cf856e
Joanna Marsden <joanna.marsden@gmail.com>
2014-04-06 21:46:03 -0400

    adjusted gemfile for heroku, changed header font to crimson

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -35,7 +35,7 @@ Refinery::Core.configure do |config|
   # config.dragonfly_secret = "67a3844bbfbf520a1c7102a8fda0000eba446b0763cb92f8"
 
   # Add extra tags to the wymeditor whitelist e.g. = {'tag' => {'attributes' => {'1' => 'href'}}} or just {'tag' => {}}
-  # config.wymeditor_whitelist_tags = {}
+  config.wymeditor_whitelist_tags = {'data-clearing' => {}}
 
   # Register extra javascript for backend
   # config.register_javascript "prototype-rails"
